4cb2385ee169ad1721091b9cd56de3d6d456f6db
Eddie <tw004@hotmail.com>
2016-08-11 20:25:40 +0800

    auto get filename

--- core.sh
+++ core.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-FPATH=stickers/$1
 
+url="https://store.line.me/stickershop/product/$1/zh-Hant"
+dirname=$(wget --no-check-certificate $url -O - \
+	     | grep 'mdCMN08Ttl' | cut -f 2 -d ">" | cut -f 1 -d "<" | tr ' ' '_')
+
+FPATH=stickers/$dirname
+	     
 if [ ! -d "$FPATH" ]; then
-mkdir $FPATH
-url="https://store.line.me/stickershop/product/$2/zh-Hant"
+	mkdir $FPATH
+	if [ ! $? ]; then
+		FPATH=stickers/$1
+		mkdir $FPATH
+	fi
+
 wget --no-check-certificate $url -O - \
 	     | grep -o 'https://sdl-stickershop.line.naver.jp/.*stickers/.*\.png' > images.txt
 wget --no-check-certificate -i images.txt
--- download.sh
+++ download.sh
@@ -4,6 +4,5 @@ FILE=list.txt
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
     line1=`echo "$line" | awk '{print $1}'`
-    line2=`echo "$line" | awk '{print $2}'`
-    ./core.sh $line1 $line2
+    ./core.sh $line1
 done < "$FILE"
